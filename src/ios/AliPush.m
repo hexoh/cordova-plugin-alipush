1bb9d82b8d2a734d7bf4d8cee0ccc09330839982
lenovo <allenxiaohu@gmail.com>
2021-10-20 10:39:52 +0800

    add android and ios platform

---
+++ src/ios/AliPush.m
@@ -0,0 +1,28 @@
+/********* AliPush.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface AliPush : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation AliPush
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
