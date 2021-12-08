2d17ae01447071ffb361485e72386237a34b1a83
lenovo <allenxiaohu@gmail.com>
2021-12-08 12:18:54 +0800

    add package.json file

--- www/AliPush.js
+++ www/AliPush.js
@@ -1,5 +1,9 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'AliPush', 'coolMethod', [arg0]);
-};
+var AliPush = {
+    coolMethod: function (arg0, success, error) {
+        exec(success, error, 'AliPush', 'coolMethod', [arg0]);
+    },
+}
+
+module.exports = AliPush;
