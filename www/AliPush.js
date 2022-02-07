d75b8c470d8ae9e7ec2f76e9e6be1227a509bdd7
lenovo <allenxiaohu@gmail.com>
2022-02-07 16:32:04 +0800

    add bind account method

--- www/AliPush.js
+++ www/AliPush.js
@@ -1,9 +1,25 @@
 var exec = require('cordova/exec');
 
 var AliPush = {
-    coolMethod: function (arg0, success, error) {
-        exec(success, error, 'AliPush', 'coolMethod', [arg0]);
-    },
+  /**
+   * bind account 绑定账号
+   * @param account account 账号
+   * @param success callbackSuccess 成功回调
+   * @param error callbackError 失败回调
+   */
+  bindAccount: function (account, success, error) {
+    exec(success, error, 'AliPush', 'bindAccount', [account]);
+  },
+
+  /**
+   * unbind account 解绑账号
+   * @param account account 账号
+   * @param success callbackSuccess 成功回调
+   * @param error callbackError 失败回调
+   */
+  unbindAccount: function (account, success, error) {
+    exec(success, error, 'AliPush', 'unbindAccount', [account]);
+  }
 }
 
 module.exports = AliPush;
